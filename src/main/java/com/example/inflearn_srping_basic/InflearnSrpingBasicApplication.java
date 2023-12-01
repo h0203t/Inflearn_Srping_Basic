8ef9fcf81af1638426449ac5a16b355ca8b3858b
hyukeh <rnjsgurxo12345@gmail.com>
2023-12-01 15:10:31 +0900

    기본

---
+++ src/main/java/com/example/inflearn_srping_basic/InflearnSrpingBasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.inflearn_srping_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSrpingBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnSrpingBasicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/inflearn_srping_basic/InflearnSrpingBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inflearn_srping_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnSrpingBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
