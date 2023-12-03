b29bc6b4b8b5359d27979cc9ee527bbc4a1247e8
hyukeh <rnjsgurxo12345@gmail.com>
2023-12-03 21:37:03 +0900

    member

---
+++ src/main/java/com/example/inflearn_srping_basic/member/Grade.java
@@ -0,0 +1,6 @@
+package com.example.inflearn_srping_basic.member;
+
+public enum Grade {
+    BASIC,
+    VIP
+}
---
+++ src/main/java/com/example/inflearn_srping_basic/member/Member.java
@@ -0,0 +1,7 @@
+package com.example.inflearn_srping_basic.member;
+
+public class Member {
+    private Long id;
+    private String name;
+    private Grade Grade;
+}
