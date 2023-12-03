6fcd38d40f73694382d9ed74b580ad567298899a
hyukeh <rnjsgurxo12345@gmail.com>
2023-12-03 22:57:48 +0900

    member

--- src/main/java/com/example/inflearn_srping_basic/member/Member.java
+++ src/main/java/com/example/inflearn_srping_basic/member/Member.java
@@ -4,4 +4,34 @@ public class Member {
     private Long id;
     private String name;
     private Grade Grade;
+
+    public Member(Long id, String name, com.example.inflearn_srping_basic.member.Grade grade) {
+        this.id = id;
+        this.name = name;
+        Grade = grade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public com.example.inflearn_srping_basic.member.Grade getGrade() {
+        return Grade;
+    }
+
+    public void setGrade(com.example.inflearn_srping_basic.member.Grade grade) {
+        Grade = grade;
+    }
 }
