40dc37a5c1195d32fa96a790c93d3ef1cb24aef2
hyukeh <rnjsgurxo12345@gmail.com>
2024-02-29 17:48:54 +0900

    AppConfig 리팩토링

--- src/main/java/com/example/inflearn_srping_basic/AppConfig.java
+++ src/main/java/com/example/inflearn_srping_basic/AppConfig.java
@@ -1,6 +1,9 @@
 package com.example.inflearn_srping_basic;
 
+import com.example.inflearn_srping_basic.discount.DiscountPolicy;
 import com.example.inflearn_srping_basic.discount.FixDiscountPolicy;
+import com.example.inflearn_srping_basic.discount.RateDiscountPolicy;
+import com.example.inflearn_srping_basic.member.MemberRepository;
 import com.example.inflearn_srping_basic.member.MemberService;
 import com.example.inflearn_srping_basic.member.MemberServiceImpl;
 import com.example.inflearn_srping_basic.member.MemoryMemberRepository;
@@ -10,12 +13,20 @@ import com.example.inflearn_srping_basic.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
         return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new FixDiscountPolicy());
+                memberRepository(),
+                discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new RateDiscountPolicy();
     }
 }
