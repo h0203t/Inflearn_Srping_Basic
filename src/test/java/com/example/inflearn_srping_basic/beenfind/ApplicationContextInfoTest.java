cd89b1d77750323f2dcd040f87e9990fa1ec5606
hyukeh <rnjsgurxo12345@gmail.com>
2024-03-06 17:31:09 +0900

    컨테이너에 등록된 모든 빈 조회

---
+++ src/test/java/com/example/inflearn_srping_basic/beenfind/ApplicationContextInfoTest.java
@@ -0,0 +1,21 @@
+package com.example.inflearn_srping_basic.beenfind;
+
+import com.example.inflearn_srping_basic.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextInfoTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("모든 빈 출력하기")
+    void findAllBenn(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("beanDefinitionName = " + beanDefinitionName + " object = " + bean);
+        }
+    }
+}
