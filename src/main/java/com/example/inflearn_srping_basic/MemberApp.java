2fb8701763a30f49b19497c139c329301ee001ad
hyukeh <rnjsgurxo12345@gmail.com>
2024-03-06 16:31:56 +0900

    스프링으로 전환하기

--- src/main/java/com/example/inflearn_srping_basic/MemberApp.java
+++ src/main/java/com/example/inflearn_srping_basic/MemberApp.java
@@ -1,15 +1,21 @@
 package com.example.inflearn_srping_basic;
 
 import com.example.inflearn_srping_basic.member.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     private static MemberRepository memberRepository;
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
 //        MemberService memberService = new MemberServiceImpl();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
--- src/main/java/com/example/inflearn_srping_basic/OrderApp.java
+++ src/main/java/com/example/inflearn_srping_basic/OrderApp.java
@@ -5,13 +5,19 @@ import com.example.inflearn_srping_basic.member.*;
 import com.example.inflearn_srping_basic.order.Order;
 import com.example.inflearn_srping_basic.order.OrderService;
 import com.example.inflearn_srping_basic.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(1L, "memberA", Grade.VIP);
