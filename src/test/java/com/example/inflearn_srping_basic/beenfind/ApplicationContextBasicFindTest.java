dae257249a300318849343710cc84d925075904f
hyukeh <rnjsgurxo12345@gmail.com>
2024-03-07 21:50:44 +0900

    스프링 빈 조회 - 기본

---
+++ src/test/java/com/example/inflearn_srping_basic/beenfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,47 @@
+package com.example.inflearn_srping_basic.beenfind;
+
+import com.example.inflearn_srping_basic.AppConfig;
+import com.example.inflearn_srping_basic.member.MemberService;
+import com.example.inflearn_srping_basic.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름 조회")
+    void findBeanByName(){
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름 없이 타입으로만 조회")
+    void findBeanByType(){
+        MemberService memberService = ac.getBean( MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByName2(){
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름으로 조회 X")
+    void findByNameX(){
+//        MemberServiceImpl memberServiceXXX = ac.getBean("memberServiceXXX", MemberServiceImpl.class);
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("memberServiceXXX", MemberServiceImpl.class));
+    }
+}
