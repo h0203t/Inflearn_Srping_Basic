4a64ebde2ad8ed17711a6b3dbd97684b2b5eabfb
hyukeh <rnjsgurxo12345@gmail.com>
2024-03-14 19:42:34 +0900

    탐색 위치와 기본 스캔 대상

--- src/main/java/com/example/inflearn_srping_basic/AutoAppConfig.java
+++ src/main/java/com/example/inflearn_srping_basic/AutoAppConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "com.example.inflearn_srping_basic.member",
+        basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
