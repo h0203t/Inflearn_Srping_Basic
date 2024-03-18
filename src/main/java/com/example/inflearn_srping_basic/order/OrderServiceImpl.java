88a2b245f8c3bf50004ff4b525e612d9a8ebf1b6
hyukeh <rnjsgurxo12345@gmail.com>
2024-03-18 15:57:17 +0900

    다양한 의존관계 주입 방법

--- src/main/java/com/example/inflearn_srping_basic/order/OrderServiceImpl.java
+++ src/main/java/com/example/inflearn_srping_basic/order/OrderServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.inflearn_srping_basic.order;
 import com.example.inflearn_srping_basic.discount.DiscountPolicy;
 import com.example.inflearn_srping_basic.member.Member;
 import com.example.inflearn_srping_basic.member.MemberRepository;
+import com.example.inflearn_srping_basic.member.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +13,15 @@ public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+//    final 삭제 해야됨
+//    @Autowired
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    } @Autowired
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//        this.discountPolicy = discountPolicy;
+//    }
+    
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
