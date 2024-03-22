ab4cc407a75f3dfac2c1378f0782d46a026e4694
hyukeh <rnjsgurxo12345@gmail.com>
2024-03-22 17:26:59 +0900

    request 스코프 예제 만들기

---
+++ src/main/java/com/example/inflearn_srping_basic/common/MyLogger.java
@@ -0,0 +1,20 @@
+package com.example.inflearn_srping_basic.common;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value = "request")
+public class MyLogger {
+
+    private String uuid;
+    private String requestURL;
+
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;
+    }
+
+    public void log(String message) {
+        System.out.println("[" + uuid + "]" + "[" + requestURL + "]" + message);
+    }
+}
