461be932a49074fa457fa7e0e8e0466402d92deb
hyukeh <rnjsgurxo12345@gmail.com>
2024-03-24 22:22:54 +0900

    request 스코프 예제 만들기

--- src/main/java/com/example/inflearn_srping_basic/common/MyLogger.java
+++ src/main/java/com/example/inflearn_srping_basic/common/MyLogger.java
@@ -1,7 +1,11 @@
 package com.example.inflearn_srping_basic.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 @Component
 @Scope(value = "request")
@@ -16,5 +20,17 @@ public class MyLogger {
 
     public void log(String message) {
         System.out.println("[" + uuid + "]" + "[" + requestURL + "]" + message);
+    }
+
+    @PostConstruct
+    public void init(){
+        String uuid = UUID.randomUUID().toString();
+        System.out.println("[" + uuid + "] request scope bean crate:" + this);
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("");
+        System.out.println("[" + uuid + "] request scope bean close:" + this);
     }
 }
